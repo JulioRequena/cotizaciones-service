353f4bf1fe225b6bc22b26408b440af88a804048
JulioRequena <36637579+JulioRequena@users.noreply.github.com>
2025-10-22 17:34:11 -0600

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,8 @@
 variable "aws_region" { default = "us-east-1" }
 variable "project" { default = "cotizaciones" }
 variable "cidr_vpc" { default = "10.0.0.0/16" }
+variable "image_tag" {
+  description = "Tag de la imagen Docker a desplegar"
+  type        = string
+}
+
